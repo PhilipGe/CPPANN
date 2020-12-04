72c7a4f64480b471da89d59ecaabac14e387477f
Philip Gessen <philip.gessen@gmail.com>
2020-12-03 22:39:21 -0500

    Created a test for network that gives a binary input and returns a decimal output (numbers 0-15)

--- include/Properties.hpp
+++ include/Properties.hpp
@@ -5,6 +5,6 @@ class Properties {
         public:
         static const int numberOfHiddenLayers = 3;
         static const int nodesPerLayer = 4;
-        static const int numberOfInputs = 3;
+        static const int numberOfInputs = 4;
         static constexpr double learningSpeed = 0.01;
 };
--- test/trainingTests.cpp
+++ test/trainingTests.cpp
@@ -7,8 +7,13 @@
 #include <eigen/Eigen/Dense>
 #include "../include/Trainer.hpp"
 
-TEST (trials, one){
+TEST (trials, DISABLED_one){
 
+    /*Properties:
+    public:
+        static const int numberOfInputs = 3;
+    */
+   
     srand(100);
     Network *net = new Network();
 
@@ -41,17 +46,114 @@ TEST (trials, one){
 
     vector<double> desiredOutputs {1,1,-1,-1,-1,-1,1,1};
 
+    Trainer::train(net,inputs,desiredOutputs,100000,true);
+
+    for(int i =0;i<inputs.size();i++){
+        EXPECT_NEAR(desiredOutputs[i],net->feedForward(inputs[i]),0.1);
+    }
+
     for(int i =0;i<inputs.size();i++){
         cout<<"Input: "<<inputs[i].transpose()<<" Output: "<<net->feedForward(inputs[i])<<" Desired: "<<desiredOutputs[i]<<endl;
     }
+}
 
-    Trainer::train(net,inputs,desiredOutputs,50000);
+TEST (trials, binary){
 
-    cout<<endl;
+    /*
+    Properties:
+    public: 
+        static const int numberOfHiddenLayers = 3;
+        static const int nodesPerLayer = 4;
+        static const int numberOfInputs = 4;
+        static constexpr double learningSpeed = 0.01;
+    */
+
+
+    Network *net = new Network();
+
+    MatrixXd inputs1(4,1);
+    inputs1 << 0, 0, 0, 1;
+    MatrixXd inputs2(4,1);
+    inputs2 << 0, 0, 1, 0;
+    MatrixXd inputs3(4,1);
+    inputs3 << 0, 0, 1, 1;
+    MatrixXd inputs4(4,1);
+    inputs4 << 0, 1, 0, 0;
+    MatrixXd inputs5(4,1);
+    inputs5 << 0, 1, 0, 1;
+    MatrixXd inputs6(4,1);
+    inputs6 << 0, 1, 1, 0;
+    MatrixXd inputs7(4,1);
+    inputs7 << 0, 1, 1, 1;
+    MatrixXd inputs8(4,1);
+    inputs8 << 1, 0, 0, 0;
+    MatrixXd inputs9(4,1);
+    inputs9 << 1, 0, 0, 1;
+    MatrixXd inputs10(4,1);
+    inputs10 << 1, 0, 1, 0;
+    MatrixXd inputs11(4,1);
+    inputs11 << 1, 0, 1, 1;
+    MatrixXd inputs12(4,1);
+    inputs12 << 1, 1, 0, 0;
+    MatrixXd inputs13(4,1);
+    inputs13 << 1, 1, 0, 1;
+    MatrixXd inputs14(4,1);
+    inputs14 << 1, 1, 1, 0;
+    MatrixXd inputs15(4,1);
+    inputs15 << 1, 1, 1, 1;
+
+
+    vector<MatrixXd> inputs;
+    inputs.push_back(inputs1);
+    inputs.push_back(inputs2);
+    // inputs.push_back(inputs3);
+    inputs.push_back(inputs4);
+    inputs.push_back(inputs5);
+    inputs.push_back(inputs6);
+    inputs.push_back(inputs7);
+    inputs.push_back(inputs8);
+    inputs.push_back(inputs9);
+    inputs.push_back(inputs10);
+    // inputs.push_back(inputs11);
+    inputs.push_back(inputs12);
+    inputs.push_back(inputs13);
+    // inputs.push_back(inputs14);
+    inputs.push_back(inputs15);
+
+    vector<double> desiredOutputs {1,2   ,4,5,6,7,8,9,10   ,12,13    ,15,16};
+    for(int i = 0;i < desiredOutputs.size();i++){
+        desiredOutputs[i] = (desiredOutputs[i]/8.0)-1;
+    }
+
+    Trainer::train(net,inputs,desiredOutputs,10000,true);
+
+    inputs.clear();
+    inputs.push_back(inputs1);
+    inputs.push_back(inputs2);
+    inputs.push_back(inputs3);
+    inputs.push_back(inputs4);
+    inputs.push_back(inputs5);
+    inputs.push_back(inputs6);
+    inputs.push_back(inputs7);
+    inputs.push_back(inputs8);
+    inputs.push_back(inputs9);
+    inputs.push_back(inputs10);
+    inputs.push_back(inputs11);
+    inputs.push_back(inputs12);
+    inputs.push_back(inputs13);
+    inputs.push_back(inputs14);
+    inputs.push_back(inputs15);
+
+    vector<double> checkDesiredOutputs {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    
+    for(int i =0;i<inputs.size();i++){
+        EXPECT_NEAR(net->feedForward(inputs[i]),(checkDesiredOutputs[i]/8.0)-1, 0.1);
+    }
 
     for(int i =0;i<inputs.size();i++){
-        cout<<"Input: "<<inputs[i].transpose()<<" Output: "<<net->feedForward(inputs[i])<<" Desired: "<<desiredOutputs[i]<<endl;
+        cout<<"Input: "<<inputs[i].transpose()<<" Output: "<<8*(net->feedForward(inputs[i])+1)<<" Desired: "<<checkDesiredOutputs[i]<<endl;
     }
+
 }
 
 int main(int argc, char **argv){
